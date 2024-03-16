d9ad43b6cd495ef8ce488ceed5fbbaa0f2970532
Mvycas <martynas.vycas@hotmail.com>
2024-03-16 19:41:30 +0100

    Navigation is done, added icons as well

---
+++ MobileApp/screens/HomeScreen.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {
+  ScrollView,
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+export default HomeScreen;
