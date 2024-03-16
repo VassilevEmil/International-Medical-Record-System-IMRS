764196933256eec8ff14fa9338c98c161499b0e0
VassilevEmil <emilvassilev5@abv.bg>
2024-03-16 19:21:17 +0100

    mongo db is set, the patient schema is done as well

---
+++ Server/src/mongo/Schemas/PatientData.ts
@@ -0,0 +1,39 @@
+import mongoose, { Schema, Model } from "mongoose";
+
+// Define patient schema
+const PatientSchema = new Schema({
+  patientId: {
+    type: String,
+    required: true,
+  },
+  patientName: {
+    type: String,
+    required: true,
+  },
+  doctorId: {
+    type: String,
+    required: true,
+  },
+  symptoms: {
+    type: String,
+    required: true,
+  },
+  diagnose: {
+    type: String,
+    required: true,
+  },
+  issuedBy: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: new Date(),
+  },
+});
+
+// Define patient model type
+
+// Export patient model
+module.exports = mongoose.model("patients", PatientSchema);
