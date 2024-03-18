28893cbb7769cb8bf74053b51f629cb6fe6987c2
VassilevEmil <emilvassilev5@abv.bg>
2024-03-18 13:20:04 +0100

    IMRS-34

--- Server/src/routes/medicalRecords.ts
+++ Server/src/routes/medicalRecords.ts
@@ -1,32 +1,52 @@
-import express, { Request, Response } from 'express';
-import { createMedicalRecord } from '../factories/medicalRecordsFactory';
-import { Encrypt } from '../services/encrypt';
-import { uploadMedicalRecord } from '../storage/ipfs';
+import express, { Request, Response } from "express";
+import { createMedicalRecord } from "../factories/medicalRecordsFactory";
+import { Encrypt } from "../services/encrypt";
+import { uploadMedicalRecord } from "../storage/ipfs";
 
 const router = express.Router();
 const encrypt = new Encrypt();
 
-
 // Post a new Medical Record
-router.post('/medicalRecord', async (req: Request, res: Response) => {
+router.post("/medicalRecord", async (req: Request, res: Response) => {
   try {
-    const { doctorFirstName, doctorLastName, sendingInstitution, diagnosisName, contentText, contentImage, language, patientId } = req.body;
-    
-    const medicalRecord = await createMedicalRecord(patientId, doctorFirstName, doctorLastName, sendingInstitution, diagnosisName, contentText, contentImage, language);
+    const {
+      doctorFirstName,
+      doctorLastName,
+      sendingInstitution,
+      diagnosisName,
+      contentText,
+      contentImage,
+      language,
+      patientId,
+      symptoms,
+      createdAt,
+    } = req.body;
+
+    const medicalRecord = await createMedicalRecord(
+      patientId,
+      doctorFirstName,
+      doctorLastName,
+      sendingInstitution,
+      diagnosisName,
+      contentText,
+      contentImage,
+      language,
+      symptoms,
+      createdAt
+    );
     console.log("MEDICAL RECORD -----", medicalRecord);
     const encryptedMedicalRecord = await encrypt.encrypt(medicalRecord);
-  
+
     const ipfsResultPaths = await uploadMedicalRecord(encryptedMedicalRecord);
-  
+
     //!! Send to dbs the ipfsResultPaths, somehow link with user
     //?? Is this a good flow? I think so :D
-  
-    res.status(201).send('Diagnosis added');
+
+    res.status(201).send("Diagnosis added");
   } catch (error) {
-    console.error('Failed to add medical record:', error);
-    res.status(500).send('Internal Server Error');
+    console.error("Failed to add medical record:", error);
+    res.status(500).send("Internal Server Error");
   }
-  
 });
 
 //  Get all Medical Records
