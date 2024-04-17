533e9e9313add9156cc1d115fcc6ce81e14700a8
VassilevEmil <emilvassilev5@abv.bg>
2024-04-17 23:55:36 +0200

    IMRS-86 Adding a test case for testing API point to add record to Mongo and publish it to ipfs.

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -1,27 +1,30 @@
-import 'dotenv/config';
-import express, { Application } from 'express';
-import { connectToDatabase } from './config/database';
-import { setupHttpsServer } from './config/httpsServer';
-import { corsMiddleware } from './middlewares/corsMiddleware';
-import medicalRecordRoutes from './routes/medicalRecords';
-import cors from 'cors'; // Import the cors middleware
-
+import "dotenv/config";
+import express, { Application } from "express";
+import {
+  connectToProductionDatabase,
+  connectToTestDatabase,
+} from "./config/database";
+import { setupHttpsServer } from "./config/httpsServer";
+import { corsMiddleware } from "./middlewares/corsMiddleware";
+import medicalRecordRoutes from "./routes/medicalRecords";
+import cors from "cors"; // Import the cors middleware
 
 const app: Application = express();
 
 // Connect to MongoDB
-connectToDatabase();
+connectToTestDatabase();
+// connectToProductionDatabase();
 
 // Middlewares
 app.use(express.json());
 app.use(cors());
 
-
 // Routes
-app.use('/medicalRecords', medicalRecordRoutes);
+app.use("/medicalRecords", medicalRecordRoutes);
 
 // Route using TLS/mTLS
 //app.use('/medicalRecords', validateClientCertificate, medicalRecordRoutes);
 
 // Setup HTTPS Server
 setupHttpsServer(app);
+export default app;
--- Server/src/config/database.ts
+++ Server/src/config/database.ts
@@ -1,13 +1,28 @@
 import mongoose from "mongoose";
 import "dotenv/config";
+import { MongoMemoryServer } from "mongodb-memory-server";
 
-export const connectToDatabase = async () => {
+export const connectToProductionDatabase = async () => {
   try {
     const mongoDBUri: string = process.env.MONGODB_URI as string;
     await mongoose.connect(mongoDBUri, {});
     console.log("Successfully connected to MongoDB.");
   } catch (err) {
     console.error("MongoDB connection error:", err);
+    process.exit(1);
+  }
+};
+
+let mongod: MongoMemoryServer;
+
+export const connectToTestDatabase = async () => {
+  try {
+    mongod = await MongoMemoryServer.create();
+    const uri = mongod.getUri();
+    await mongoose.connect(uri, {});
+    console.log("Successfully connected to MongoMemoryServer.");
+  } catch (err) {
+    console.error("MongoMemoryServer connection error:", err);
     process.exit(1);
   }
 };
