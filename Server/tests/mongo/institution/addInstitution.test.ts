ed2580215d9de676fd05e1a774ecd004d7daca76
Simas <simas.safr@gmail.com>
2024-04-24 12:15:51 +0200

    refactor of tests for less boiler plate

--- Server/tests/mongo/institution/addInstitution.test.ts
+++ Server/tests/mongo/institution/addInstitution.test.ts
@@ -7,11 +7,20 @@ import { Country } from '../../../src/enums';
 
 describe('addInstitution', () => {
   let mongod: MongoMemoryServer;
+  let institutionData: Institution;
 
   beforeAll(async () => {
     mongod = await MongoMemoryServer.create();
     const uri = mongod.getUri();
     await mongoose.connect(uri);
+
+    institutionData = {
+      id: 'testId123',
+      institutionId: 'testInstitutionId123',
+      name: 'Test Institution Name',
+      country: Country.Denmark,
+      address: 'Test Institution Address'
+    };
   });
 
   afterAll(async () => {
@@ -20,74 +29,28 @@ describe('addInstitution', () => {
   });
 
   test('successfully adds an institution and verify by retrieval', async () => {
-    const institutionData: Institution = {
-      id: 'testId123',
-      institutionId: 'testInstitutionId123',
-      name: 'Test Institution Name',
-      country: Country.Denmark,
-      address: 'Test Institution Address'
-    };
-    
     await addInstitution(institutionData);
-
     const savedInstitution = await InstitutionModel.findOne({ institutionId: 'testInstitutionId123' });
     
-    if(savedInstitution){
-        expect(savedInstitution.id).toBe(institutionData.id);
-        expect(savedInstitution.institutionId).toBe(institutionData.institutionId);
-        expect(savedInstitution.name).toBe(institutionData.name);
-        expect(savedInstitution.country).toBe(institutionData.country);
-        expect(savedInstitution.address).toBe(institutionData.address);
-      }
+    expect(savedInstitution).toMatchObject({
+      id: institutionData.id,
+      institutionId: institutionData.institutionId,
+      name: institutionData.name,
+      country: institutionData.country,
+      address: institutionData.address
+    });
   });
 
   describe('Error throws for empty variables', () => {
-    test('id empty', async () => {
-      const invalidInstitutionData = {
-        id: '',
-        institutionId: 'testInstitutionId123',
-        name: 'Valid Institution Name',
-        country: Country.Denmark,
-        address: 'Test Institution Address'
-      };
-
-      await expect(addInstitution(invalidInstitutionData)).rejects.toThrow();
+    test.each([
+      [{...institutionData, id: ''}, 'id missing'],
+      [{...institutionData, institutionId: ''}, 'institutionId missing'],
+      [{...institutionData, name: ''}, 'name missing'],
+      [{...institutionData, address: ''}, 'address missing'],
+    ])('throws an error when %s is empty', async (invalidData) => {
+      await expect(addInstitution(invalidData))
+        .rejects
+        .toThrow();
     });
-
-    test('institutionId empty', async () => {
-      const invalidInstitutionData = {
-        id: 'testId123',
-        institutionId: '',
-        name: 'Valid Institution Name',
-        country: Country.Denmark,
-        address: 'Test Institution Address'
-      };
-
-      await expect(addInstitution(invalidInstitutionData)).rejects.toThrow();
-    });
-
-    test('name empty', async () => {
-      const invalidInstitutionData = {
-        id: 'testId123',
-        institutionId: 'testInstitutionId123',
-        name: '',
-        country: Country.Denmark,
-        address: 'Test Institution Address'
-      };
-
-      await expect(addInstitution(invalidInstitutionData)).rejects.toThrow();
-    });
-
-    test('address empty', async () => {
-      const invalidInstitutionData = {
-        id: 'testId123',
-        institutionId: 'testInstitutionId123',
-        name: 'Valid Institution Name',
-        country: Country.Denmark,
-        address: ''
-      };
-
-      await expect(addInstitution(invalidInstitutionData)).rejects.toThrow();
-    });
-  })
+  });
 });
