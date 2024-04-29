3f858c67c3ba569f07c7accf695e6106f6cef3fb
VassilevEmil <emilvassilev5@abv.bg>
2024-04-29 11:55:30 +0200

    added a proper comment in 'addRecord.test' . getRecord.test is partly working.  Will work on it later.

--- Server/src/test/getRecord.test.ts
+++ Server/src/test/getRecord.test.ts
@@ -2,61 +2,114 @@ import request from "supertest";
 
 import app from "../app";
 import * as medicalRecordController from "../mongo/controllers/medicalRecordController";
-import { Language, TypeOfRecord } from "../enums";
+import { Country, Language, TypeOfRecord } from "../enums";
 import { Institution } from "../models/institution";
 
+import { getMedicalRecordById } from "../mongo/controllers/medicalRecordController";
+
 import mongoose from "mongoose";
-import { id } from "ethers/lib/utils";
-import InstitutionModel from "../mongo/models/institution";
+
+import MedicalRecordReferenceModel from "../mongo/models/medicalRecordReference";
+import { FileInfo } from "../models/fileInfo";
 
 afterAll(async () => {
   await mongoose.disconnect();
 });
 
-// this one doesnt pass, need to work on it more.  i will commit anyway for now so you guys can fetch
+// getRecordById  but in order to get it firstly i need to create it
 
-describe("get record by id", () => {
-  describe("get document rounte", () => {
-    describe("given the document exist in  database", () => {
-      it("should return a valid response with status 201", async () => {
-        const medicalRecord = {
-          id: "12",
-          patientId: "456",
-          doctorId: "789",
-          doctorFirstName: "John",
-          doctorLastName: "Doe",
-          institution: { id: "123", name: "hospital name" } as Institution,
-          timeStamp: new Date(),
-          language: Language.English,
-          title: "Test Medical Record",
-          text: ["dssds"],
-          typeOfRecord: TypeOfRecord.Bloodwork,
-        };
-
-        // want to see whats here
-        console.log(medicalRecordController, "medicalrecordcontroller");
-
+describe("add record and fetch it by id", () => {
+  describe("get document route", () => {
+    describe("given the document has been added", () => {
+      it("should return a 201 if successfully adds a record", async () => {
         jest.mock("../mongo/controllers/medicalRecordController", () => ({
           ...jest.requireActual("../mongo/controllers/medicalRecordController"),
-          getMedicalRecordById: jest.fn(),
+          getMedicalRecordById: jest.fn(async (medicalRecordId: string) => {
+            if (medicalRecordId === "123") {
+              return {
+                id: "123",
+                patientId: "123",
+                institution: {
+                  id: "123",
+                  name: "hospital name",
+                },
+                typeOfRecord: TypeOfRecord.Bloodwork,
+                medicalRecordHash: "ipfs://hash123",
+                fileHash: [],
+                timeStamp: new Date(),
+              };
+            } else {
+              throw new Error(
+                `Medical Record Reference not found with ID: ${medicalRecordId}`
+              );
+            }
+          }),
         }));
-        const mockedGetMedicalRecordById =
-          medicalRecordController.getMedicalRecordById as jest.MockedFunction<
-            typeof medicalRecordController.getMedicalRecordById
-          >;
 
-        mockedGetMedicalRecordById.mockResolvedValue(medicalRecord);
+        const fileHash: FileInfo[] = [
+          {
+            id: "1",
+            name: "file1.jpg",
+            mimetype: "image/jpeg",
+            fileHash: "hash1",
+          },
+          {
+            id: "2",
+            name: "file2.jpg",
+            mimetype: "image/jpeg",
+            fileHash: "hash2",
+          },
+        ];
 
-        const response = await request(app).get(
-          `/medicalRecords/${medicalRecord.id}`
-        );
+        const medicalRecord = {
+          id: "123",
+          patientId: "123",
+          institution: {
+            id: "123",
+            name: "hospital name",
+            institutionId: "123",
+            country: Country.Bulgaria,
+            address: "random",
+          } as Institution,
+          typeOfRecord: TypeOfRecord.Bloodwork,
+          medicalRecordHash: "4df3g53c3",
+          fileHash: fileHash,
+          timeStamp: new Date(),
+        };
 
-        // Assert the response status code
+        jest.mock("../storage/ipfs", () => ({
+          ...jest.requireActual("../storage/ipfs"),
+          getMedicalRecordFromIpfs: jest.fn(async (ipfsUrl: string) => {
+            const ipfsHash = ipfsUrl.replace("ipfs://", "");
+
+            console.log("haaaaaaaaaaaashh: ", ipfsHash);
+
+            if (ipfsHash === "hash123") {
+              return {
+                id: "123",
+                patientId: "123",
+                institution: {
+                  id: "123",
+                  name: "hospital name",
+                },
+                typeOfRecord: TypeOfRecord.Bloodwork,
+                medicalRecordHash: "ipfs://hash123",
+                fileHash: [],
+                timeStamp: new Date(),
+              };
+            } else {
+              throw new Error("Medical record not found in IPFS");
+            }
+          }),
+        }));
+
+        await MedicalRecordReferenceModel.create(medicalRecord);
+
+        const response = await request(app).get("/medicalRecords/123");
         expect(response.status).toBe(200);
 
-        // Assert the response body
         expect(response.body).toEqual(medicalRecord);
-      });
+      }, 10000); // adding this because it was failing, timeout..
     });
   });
 });
