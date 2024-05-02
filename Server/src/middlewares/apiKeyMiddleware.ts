60c6fccd934400b24082b78ad394cfca9fe2a5a8
Simas <simas.safr@gmail.com>
2024-05-02 18:03:02 +0200

    removed log

--- Server/src/middlewares/apiKeyMiddleware.ts
+++ Server/src/middlewares/apiKeyMiddleware.ts
@@ -4,7 +4,6 @@ export function apiKeyMiddleware(req: Request, res: Response, next: NextFunction
   const apiKey = req.headers['x-api-key'];
 
   if (apiKey === process.env.API_KEY) {
-    console.log("Correct API Key");
     next();
   } else {
     res.status(401).json({ error: 'Unauthorized access: Invalid API key' });
